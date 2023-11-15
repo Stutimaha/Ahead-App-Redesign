8d4fced7c81c8ff324651175bbaba451b5fa6d6f
Stutimaha <94779689+Stutimaha@users.noreply.github.com>
2023-11-15 12:05:38 +0530

    Update WorkWithUs.jsx

--- src/components/WorkWithUs.jsx
+++ src/components/WorkWithUs.jsx
@@ -38,7 +38,7 @@ const WorkWithUs = () => {
           <div className="grow">
             <div className="rounded-2xl bg-white  pt-8 max-w-[30rem] shadow-lg">
               <motion.div
-              className="pl-4 inline-block"
+                className="pl-4 inline-block"
                 initial="hidden"
                 whileInView="visible"
                 viewport={{ once: true }}
@@ -53,7 +53,7 @@ const WorkWithUs = () => {
               <div className="flex flex-col gap-3 p-4">
                 <h2 className="text-xl font-bold">About</h2>
                 <p className="font-normal">
-                 At ahead our goal is to make self- improvement fun and lasting. We know there's a way how to make it work. And that's what aHead is all about!
+                  At ahead our goal is to make self- improvement fun and lasting. We know there&apos;s a way how to make it work. And that&apos;s what aHead is all about!
                 </p>
               </div>
 
@@ -72,7 +72,7 @@ const WorkWithUs = () => {
                 key={index}
               >
                 <h2 className="font-bold">
-                 Power through, even when the going gets tough
+                  Power through, even when the going gets tough
                 </h2>
                 <p className="font-normal">
                   We are a team of passionate people who love to build amazing products. We really love our work and we do it with pleasure.
